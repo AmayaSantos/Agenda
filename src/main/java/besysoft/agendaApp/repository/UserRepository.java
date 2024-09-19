0aff6218583b92555cfa32553c012a283f34db69
emanuel.amaya <emanuelamaya@julasoft.com>
2024-09-19 05:26:12 -0700

    fix findByEmail

---
+++ src/main/java/besysoft/agendaApp/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package besysoft.agendaApp.repository;
+
+import besysoft.agendaApp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
