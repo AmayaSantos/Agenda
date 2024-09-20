4cb7a34540060234ec9d60bdbdc5fb709295f136
emanuel.amaya <emanuelamaya@julasoft.com>
2024-09-20 02:52:56 -0700

    clean code

--- src/main/java/besysoft/agendaApp/dto/PersonFilterDto.java
+++ src/main/java/besysoft/agendaApp/dto/PersonFilterDto.java
@@ -1,9 +1,9 @@
 package besysoft.agendaApp.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
