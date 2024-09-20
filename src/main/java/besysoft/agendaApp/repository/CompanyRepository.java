54bf1f0fe8ae002a5a07f1343e78bfeb750f2344
emanuel.amaya <emanuelamaya@julasoft.com>
2024-09-19 17:10:37 -0700

    update interface to Company and Person

--- src/main/java/besysoft/agendaApp/repository/CompanyRepository.java
+++ src/main/java/besysoft/agendaApp/repository/CompanyRepository.java
@@ -1,10 +1,11 @@
 package besysoft.agendaApp.repository;
 
-import besysoft.agendaApp.model.Person;
+import besysoft.agendaApp.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<Person, Integer> {
+public interface CompanyRepository extends JpaRepository<Company, Integer>, JpaSpecificationExecutor<Company> {
 
 }
