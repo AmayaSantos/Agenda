5e4b9eb59c72ecb5ff1b9990afe1a91e595004df
emanuel.amaya <emanuelamaya@julasoft.com>
2024-09-23 10:52:00 -0700

    clean code and improve method  fullName in PersonSpecification

--- src/main/java/besysoft/agendaApp/repository/specifications/PersonSpecification.java
+++ src/main/java/besysoft/agendaApp/repository/specifications/PersonSpecification.java
@@ -13,24 +13,23 @@ import static io.micrometer.common.util.StringUtils.isBlank;
 
 public class PersonSpecification {
 
+    private static final String SURNAME = "surname";
+    private static final String CITY = "city";
+    private static final String NAME = "name";
+
     public static Specification<Person> fullName(String fullName) {
         return (root, query, builder) -> (isBlank(fullName)) ? builder.conjunction():
-            builder.equal(getUpperSaveFullName(root, builder), fullName.toUpperCase());
-
+            builder.like(getUpperSaveFullName(root, builder), "%"+fullName.trim().toUpperCase()+"%");
     }
 
     private static Expression<String> getUpperSaveFullName(Root<Person> root, CriteriaBuilder builder) {
         return builder.upper(
-                builder.concat(
-                        builder.concat(root.get("name"), " "),
-                        root.get("surname")
-                )
-        );
+                builder.concat(root.get(NAME), root.get(SURNAME)));
     }
 
     public static Specification<Person> inCities(List<String> cities) {
         return (root, query, builder) ->(cities.isEmpty()) ? builder.conjunction():
-                builder.upper(root.get("city")).in( getUpperCaseCities(cities));
+                builder.upper(root.get(CITY)).in( getUpperCaseCities(cities));
     }
 
     private static List<String> getUpperCaseCities(List<String> cities) {
